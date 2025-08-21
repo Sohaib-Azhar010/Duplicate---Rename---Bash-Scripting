90b1780dd04777e5a59758f1dcd1bdda6e8cb284
Sohaib Azhar <sohaibazhar04@gmail.com>
2025-08-21 13:22:05 +0500

    updated some chunks and comments

--- duplicate_rename.sh
+++ duplicate_rename.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-# filepath: duplicate_rename.sh  ./myfile.txt
+# filepath: duplicate_rename.sh  ./myfile.txt (It is the first argument)
 
 # Check if argument is given
+# $# mean no. of arguments while bashing 
 if [ $# -eq 0 ]; then
   echo "File path not given"
   echo "Usage: $0 <file-path>"
   exit 1
 fi
 
+# $1 = first argument (the file path you provided).
+# Stored in variable SRC_FILE.
 SRC_FILE="$1"
 
 # Check if file exists
@@ -18,23 +21,23 @@ fi
 
 # Get filename and extension
 FILENAME=$(basename -- "$SRC_FILE")
-EXT="${FILENAME##*.}"
+EXTENSION="${FILENAME##*.}"    # extracting extension of file from last dot
 NAME="${FILENAME%.*}"
 
-# Set Desktop path (Windows Git Bash or WSL)
+# Set Desktop path 
 DESKTOP="$HOME/Desktop"
 
-# Copy file to Desktop
+# Copy original file to Desktop
 cp "$SRC_FILE" "$DESKTOP/$FILENAME"
 
 # Create two duplicates with new names
-cp "$DESKTOP/$FILENAME" "$DESKTOP/${NAME}_copy1.$EXT"
-cp "$DESKTOP/$FILENAME" "$DESKTOP/${NAME}_copy2.$EXT"
+cp "$DESKTOP/$FILENAME" "$DESKTOP/${NAME}_copy1.$EXTENSION"
+cp "$DESKTOP/$FILENAME" "$DESKTOP/${NAME}_copy2.$EXTENSION"
 
 # Delete original file from source folder
 rm "$SRC_FILE"
 
 echo "Done! Files created on Desktop:"
 echo "$FILENAME"
-echo "${NAME}_copy1.$EXT"
-echo "${NAME}_copy2.$EXT"
+echo "${NAME}_copy1.$EXTENSION"
+echo "${NAME}_copy2.$EXTENSION"
